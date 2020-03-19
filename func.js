a5494ef8b3a53ca1ab572da9e2f2c9074695c646
Wootom <test@test.test>
2020-03-19 21:43:47 +0900

    첫번째 commit

---
+++ func.js
@@ -0,0 +1,9 @@
+var num=0; /* var 변수를 입력할때 쓴다. 따로 타입은 지정하지 않는다. 생략 가능하다 */
+console.log(num)
+document.write("head 태그 내 실행 순서 : "+num+"<br>") /*  document: 현재 페이지 */
+document.write("head 태그 내 실행 순서 : "+num+"<br>")
+var age=23
+document.write("<div style='color:red; font-size:24px;'>"
+	+"자바 스크립트</div>") /* jsp java 내용 다 사용할수 있다. */
+document.write("<div style=\"color:red; font-size:24px;\">"
+	+"자바 스크립트</div>")	
